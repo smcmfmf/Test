1eddc95163a735c8e758052c1c00e342c3e672ac
smcmfmf <hk0831bae@gmail.com>
2025-03-12 11:46:37 +0900

    Initial commit

---
+++ src/test/java/kr/ac/kopo/smcmfmf/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.smcmfmf.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
